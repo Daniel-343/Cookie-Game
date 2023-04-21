63e41594514d12c0e9153e60983465669c41f884
Daniel-Toth <danthe34@gmail.com>
2023-04-22 01:46:13 +0200

    add randomly falling cookie

--- client/public/script.js
+++ client/public/script.js
@@ -4,7 +4,14 @@ let score = 0;
 scoreDisplay.insertAdjacentHTML('afterbegin', `score: ${score}`);
 
 mainButton.addEventListener('click', function (event) {
+  let windowWidth = window.innerWidth;
+document.documentElement.style.setProperty('--windowsize', windowWidth);
+console.log(windowWidth);
   score++;
   scoreDisplay.innerHTML = '';
   scoreDisplay.insertAdjacentHTML('afterbegin', `score: ${score}`);
 });
+
+let randomValue = Math.random();
+document.documentElement.style.setProperty('--random', randomValue);
+
