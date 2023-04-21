61091bade926f4a70a4d5b328cce1270a2d7c36c
Daniel-Toth <danthe34@gmail.com>
2023-04-21 23:18:57 +0200

    add score counter, add css to score counter

--- client/public/script.js
+++ client/public/script.js
@@ -1,1 +1,10 @@
+const mainButton = document.getElementById('mainCookie');
+const scoreDisplay = document.getElementById('score');
+let score = 0;
+scoreDisplay.insertAdjacentHTML('afterbegin', `score: ${score}`);
 
+mainButton.addEventListener('click', function (event) {
+  score++;
+  scoreDisplay.innerHTML = '';
+  scoreDisplay.insertAdjacentHTML('afterbegin', `score: ${score}`);
+});
