c6fce8316412fad829bba028bbaa561f7137b44c
Daniel-Toth <danthe34@gmail.com>
2023-04-23 13:12:52 +0200

    fix falling cookies

--- client/public/script.js
+++ client/public/script.js
@@ -2,16 +2,23 @@ const mainButton = document.getElementById('mainCookie');
 const scoreDisplay = document.getElementById('score');
 let score = 0;
 scoreDisplay.insertAdjacentHTML('afterbegin', `score: ${score}`);
+let counter = 0;
 
-mainButton.addEventListener('click', function (event) {
-  let windowWidth = window.innerWidth;
-document.documentElement.style.setProperty('--windowsize', windowWidth);
-console.log(windowWidth);
-  score++;
-  scoreDisplay.innerHTML = '';
-  scoreDisplay.insertAdjacentHTML('afterbegin', `score: ${score}`);
+mainButton.addEventListener('click', function () {
+  counter++;
+  const cookie = document.createElement('div');
+  cookie.classList.add('cookie');
+  cookie.style.left = `${Math.random() * window.innerWidth}px`;
+  document.body.appendChild(cookie);
+  setTimeout(() => {
+    cookie.style.top = `${window.innerHeight + 50}px`;
+    setTimeout(() => {
+      document.body.removeChild(cookie);
+      counter--;
+    }, 1000);
+  }, 100);
+
+  console.log(counter);
 });
 
-let randomValue = Math.random();
-document.documentElement.style.setProperty('--random', randomValue);
 
